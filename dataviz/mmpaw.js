9476a966440bb458f657e494719dd1daf662fb97
Erkan <erkangunes39@gmail.com>
2020-11-30 21:12:21 -0700

    Update mmpaw.js

--- dataviz/mmpaw.js
+++ dataviz/mmpaw.js
@@ -24,7 +24,7 @@ function setup() {
 
   var countrySelect = createSelect();
   countrySelect.position(windowWidth*0.026, windowHeight*0.93);
-  //countrySelect.parent('mysketch');
+  countrySelect.parent('mysketch');
   countryOptions = protestVolume.getColumn('country');
   uniqueItems = Array.from(new Set(countryOptions));
   uniqueItems = uniqueItems.sort();
